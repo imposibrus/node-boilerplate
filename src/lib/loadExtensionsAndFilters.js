5ce796c6e5e491926043c795bd45d4a72362ba0e
imposibrus <imposibrus@gmail.com>
2017-09-21 18:30:22 +0300

    remove pug from dependencies improve Nunjucks extensions and filters loading

--- src/lib/loadExtensionsAndFilters.js
+++ src/lib/loadExtensionsAndFilters.js
@@ -1,8 +1,30 @@
 
-const SessionFlashExtension = require('./extensions/SessionFlashExtension'),
-    basename = require('./filters/basename');
+const path = require('path'),
+    requireAll = require('require-all'),
+    extensions = requireAll(path.join(__dirname, './extensions')),
+    filters = requireAll(path.join(__dirname, './filters'));
 
 module.exports = function(env, nunjucks) {
-    SessionFlashExtension(env, nunjucks);
-    basename(env);
+    for (const extName in extensions) {
+        if (!Object.prototype.hasOwnProperty.call(extensions, extName)) {
+            continue;
+        }
+
+        const extension = extensions[extName];
+
+        if (typeof extension === 'function') {
+            extension(env, nunjucks);
+        }
+    }
+    for (const filterName in filters) {
+        if (!Object.prototype.hasOwnProperty.call(filters, filterName)) {
+            continue;
+        }
+
+        const filter = filters[filterName];
+
+        if (typeof filter === 'function') {
+            filter(env, nunjucks);
+        }
+    }
 };
